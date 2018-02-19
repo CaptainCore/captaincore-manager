2a4905b57336eea2c59250446b36ad2224944f4b
Austin Ginder <austinginder@gmail.com>
2018-02-18 21:24:37 -0500

    Cache bust

--- public/class-captaincore-public.php
+++ public/class-captaincore-public.php
@@ -73,7 +73,7 @@ class Captaincore_Public {
 		 * class.
 		 */
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/captaincore-public.20180214.css', array(), $this->version, 'all' );
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/captaincore-public.2018-02-18.css', array(), $this->version, 'all' );
 		wp_enqueue_style( 'google-material-icons', "https://fonts.googleapis.com/icon?family=Material+Icons", array(), '2017-09-03' );
 		wp_enqueue_style( 'materialize', plugin_dir_url( __FILE__ ) . "css/materialize-src/materialize.css", array(), '2017-09-08' );
 		wp_enqueue_script( 'font-awesome', "https://use.fontawesome.com/releases/v5.0.6/js/all.js", array() );
