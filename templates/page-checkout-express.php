0284427e6156e0ba8c63dd0eff1ccab7b0ac169a
Austin Ginder <austinginder@gmail.com>
2018-04-27 16:22:11 -0400

    Whitespace update

--- templates/page-checkout-express.php
+++ templates/page-checkout-express.php
@@ -3,7 +3,7 @@
  * Template Name: Checkout Express
  */
 
-$order_id = get_query_var('callback');
+$order_id = get_query_var( 'callback' );
 $key      = $_GET['key'];
 
 // Loads user and key from order
