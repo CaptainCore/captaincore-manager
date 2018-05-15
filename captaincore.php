4c30c0aa402f0316880081b23a72ddbf648761bd
Austin Ginder <austinginder@gmail.com>
2018-05-15 19:44:37 -0400

    B2 Snapshots now configurable from wp-config CAPTAINCORE_B2_SNAPSHOTS

--- captaincore.php
+++ captaincore.php
@@ -3544,6 +3544,7 @@ function anchor_download_snapshot_email( $snapshot_id ) {
 	$website    = get_field( 'website', $snapshot_id );
 	$website_id = $website[0];
 	$domain     = get_the_title( $website_id );
+	$site       = get_field( 'site', $website_id );
 
 	// Get new auth from B2
 	$account_id      = CAPTAINCORE_B2_ACCOUNT_ID; // Obtained from your B2 account page
@@ -3567,10 +3568,10 @@ function anchor_download_snapshot_email( $snapshot_id ) {
 
 	// Variables for Backblaze
 	$api_url          = 'https://api001.backblazeb2.com'; // From b2_authorize_account call
-	$auth_token       = $output->authorizationToken; // From b2_authorize_account call
-	$bucket_id        = CAPTAINCORE_B2_BUCKET_ID; // The file name prefix of files the download authorization will allow
-	$valid_duration   = 604800; // The number of seconds the authorization is valid for
-	$file_name_prefix = 'Snapshots/' . $domain; // The file name prefix of files the download authorization will allow
+	$auth_token       = $output->authorizationToken;      // From b2_authorize_account call
+	$bucket_id        = CAPTAINCORE_B2_BUCKET_ID;         // The file name prefix of files the download authorization will allow
+	$valid_duration   = 604800;                           // The number of seconds the authorization is valid for
+	$file_name_prefix = 'Snapshots/' . $site;             // The file name prefix of files the download authorization will allow
 
 	$session = curl_init( $api_url . '/b2api/v1/b2_get_download_authorization' );
 
@@ -3591,10 +3592,9 @@ function anchor_download_snapshot_email( $snapshot_id ) {
 	curl_setopt( $session, CURLOPT_RETURNTRANSFER, true );  // Receive server response
 	$server_output = curl_exec( $session ); // Let's do this!
 	curl_close( $session ); // Clean up
-	// echo ($server_output); // Tell me about the rabbits, George!
 	$server_output = json_decode( $server_output );
-	$auth = $server_output->authorizationToken;
-	$url = "https://f001.backblazeb2.com/file/AnchorHostBackup/Snapshots/$domain/$name?Authorization=" . $auth;
+	$auth          = $server_output->authorizationToken;
+	$url           = "https://f001.backblazeb2.com/file/" .CAPTAINCORE_B2_SNAPSHOTS . "/$site/$name?Authorization=" . $auth;
 
 	echo $url;
 
