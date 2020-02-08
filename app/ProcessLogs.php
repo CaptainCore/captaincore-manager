f7a829c9a2900eb911d2fc12f7a47107e919493b
Austin Ginder <austinginder@gmail.com>
2020-02-08 13:56:05 -0500

    🐛 FIX: All process logs

--- app/ProcessLogs.php
+++ app/ProcessLogs.php
@@ -6,4 +6,15 @@ class ProcessLogs extends DB {
 
 	static $primary_key = 'process_log_id';
 
+	public function list() {
+
+		$results = [];
+		$process_log_ids = self::select_all( 'process_log_id' );
+		foreach ( $process_log_ids as $process_log_id ) {
+			$results[] = ( new ProcessLog( $process_log_id ) )->get();
+		}
+		return $results;
+
+	}
+
 }
