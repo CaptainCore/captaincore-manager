b7de03b1bf151d8994dc6e7b63a5c048a0b3b6e5
Austin Ginder <austinginder@gmail.com>
2020-03-14 19:56:07 -0400

    📦 NEW: Account sync

--- app/Account.php
+++ app/Account.php
@@ -47,6 +47,36 @@ class Account {
 
     }
 
+    public function sync() {
+
+        $command = "account sync {$this->account_id}";
+        
+        // Disable https when debug enabled
+        if ( defined( 'CAPTAINCORE_DEBUG' ) ) {
+            add_filter( 'https_ssl_verify', '__return_false' );
+        }
+
+        $data = [ 
+            'timeout' => 45,
+            'headers' => [
+                'Content-Type' => 'application/json; charset=utf-8', 
+                'token'        => CAPTAINCORE_CLI_TOKEN 
+            ],
+            'body'        => json_encode( [ "command" => $command ]), 
+            'method'      => 'POST', 
+            'data_format' => 'body'
+        ];
+
+        // Add command to dispatch server
+        $response = wp_remote_post( CAPTAINCORE_CLI_ADDRESS . "/run", $data );
+        if ( is_wp_error( $response ) ) {
+            $error_message = $response->get_error_message();
+            return "Something went wrong: $error_message";
+        }
+        
+        return $response["body"];
+    }
+
     public function assign_sites( $site_ids = [] ) {
 
         $accountsite = new AccountSite();
