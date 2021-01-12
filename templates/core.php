3c879b27772af16f6850d47ea0a94867bcdd9268
Austin Ginder <austinginder@gmail.com>
2021-01-12 15:17:40 -0500

    🐛 FIX: Screenshot bug

--- templates/core.php
+++ templates/core.php
@@ -2177,7 +2177,7 @@ $user = wp_get_current_user();
 							<v-layout body-1 px-6 class="row">
 								<v-flex xs12 md6 class="py-2">
 								<div class="block mt-6">
-                            		<a @click="showCaptures( dialog_site.site.site_id )"><v-img :src="dialog_site.environment_selected.screenshots.large" max-width="400" aspect-ratio="1.6" class="elevation-5" v-show="dialog_site.environment_selected.screenshots.large" style="margin:auto;"></v-img></a>
+                            		<a @click="showCaptures( dialog_site.site.site_id )"><v-img :src="dialog_site.environment_selected.screenshots.large" max-width="400" aspect-ratio="1.6" class="elevation-5" v-if="typeof dialog_site.environment_selected.screenshots != 'undefined' && dialog_site.environment_selected.screenshots.large" style="margin:auto;"></v-img></a>
 								</div>
 								<v-list dense style="padding:0px;max-width:350px;margin: auto;" class="mt-6">
 									<v-list-item :href="dialog_site.environment_selected.link" target="_blank" dense>
