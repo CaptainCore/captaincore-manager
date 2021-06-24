e8984cd8a691dfee5ff8ed251d58cab386005173
Austin Ginder <austinginder@gmail.com>
2021-06-24 14:28:13 -0400

    📦 NEW: Add Fathom Analytics site

--- vendor/beyondcode/fathom-api/src/FathomAnalytics.php
+++ vendor/beyondcode/fathom-api/src/FathomAnalytics.php
@@ -25,8 +25,9 @@ class FathomAnalytics
     public function __construct($email, $password)
     {
         $this->browser = new HttpBrowser(HttpClient::create());
-
         $this->login($email, $password);
+        $this->email    = $email;
+        $this->password = $password;
     }
 
     protected function login($email, $password, $oneTimePassword = null)
@@ -34,7 +35,7 @@ class FathomAnalytics
         $this->browser->request('GET', static::LOGIN_URL);
 
         $this->browser->submitForm('Log in', [
-            'email' => $email,
+            'email'    => $email,
             'password' => $password
         ]);
 
@@ -73,5 +74,68 @@ class FathomAnalytics
 
         $this->browser->request('GET', "https://app.usefathom.com/sites/{$siteId}/data?from={$from->toDateTimeString()}&to={$to->toDateTimeString()}&site={$siteId}&range=today&tz=Europe%2FBerlin");
         return json_decode($this->browser->getResponse()->getContent());
+    }
+
+    public function newSite($domain)
+    {
+        $sites = self::getSites();
+        foreach( $sites as $site ) {
+            if ( $site->name == $domain ) {
+                return (object) [ 
+                    "tracking_id"    => $site->tracking_id,
+                    "name"           => $domain,
+                    "response"       => "Tracking already exists for $domain",
+                ];
+            }
+        }
+
+        $login = wp_remote_get( "https://app.usefathom.com/login" );
+        if ( is_wp_error( $login ) ) {
+            echo "Error: Couldn't retrieve Fathom login page\n";
+            return;
+        }
+        preg_match( '/_token\" value=\"(.+)\"/', $login['body'],  $token );
+
+        $auth = wp_remote_post( "https://app.usefathom.com/login", [ 
+            'method'  => 'POST', 
+            'cookies' => $login['cookies'],
+            'headers' => [ 'Content-Type' => 'application/json; charset=utf-8' ],
+            'body'    => json_encode( [ 
+                "_token" => $token[1],
+                "email"    => $this->email,
+                "password" => $this->password,
+                "remember" => "on" ] )
+        ] );
+        if ( is_wp_error( $auth ) ) {
+            echo "Error: Couldn't login to Fathom\n";
+            return;
+        }
+        $home = wp_remote_get( "https://app.usefathom.com", [ 'method'  => 'GET', 'cookies' => $auth['cookies'] ] );
+        if ( is_wp_error( $home ) ) {
+            echo "Error: Couldn't fetch Fathom's home page\n";
+            return;
+        }
+        preg_match( '/window\.xsrf_token = \"(.*)\"/', $home['body'], $token );
+
+        $response = wp_remote_post( "https://app.usefathom.com/sites", [ 
+            'method'  => 'POST',
+            'cookies' => $home['cookies'],
+            'headers' => [ 'Content-Type' => 'application/json; charset=utf-8', "x-csrf-token" => $token[1] ],
+            'body' => json_encode( [ 
+                "site" => [ 
+                    "tracking_id"    => null,
+                    "name"           => $domain,
+                    "sharing"        => "none",
+                    "share_url"      => null, 
+                    "share_password" => null,
+                    "monitor"        => null
+                ]
+            ] )
+        ] );
+        if ( is_wp_error( $response ) ) {
+            echo "Error: Couldn't fetch Fathom's response\n";
+            return;
+        }
+        return json_decode( $response['body'] )->Site;
     }
 }
