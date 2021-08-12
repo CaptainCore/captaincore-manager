21bc006b95a007049e8d262a250262cf6149849d
Austin Ginder <austinginder@gmail.com>
2021-08-12 19:29:11 -0400

    🐛 FIX: Deactivate

--- captaincore.php
+++ captaincore.php
@@ -5217,7 +5217,7 @@ function captaincore_install_action_callback() {
 	}
 	if ( $cmd == 'deactivate' ) {
 		$run_in_background = true;
-		$command           = "deactivate $site -- --name=\"$name\" --link=\"$link\"";
+		$command           = "deactivate $site --name=\"$name\" --link=\"$link\"";
 	}
 	if ( $cmd == 'activate' ) {
 		$run_in_background = true;
