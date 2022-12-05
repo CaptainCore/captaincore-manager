c6a2777a4dc163b6f45434e46a6d911f0fd8b3f9
Austin Ginder <austinginder@gmail.com>
2022-12-04 20:14:25 -0500

    🐛 FIX: Remove composer platform check

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit9f7509cc1c55bc410ccf6f05510f2050
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit9f7509cc1c55bc410ccf6f05510f2050', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit9f7509cc1c55bc410ccf6f05510f2050', 'loadClassLoader'));
