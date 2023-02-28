46f146204cd73c17194b9f9fae18c034a4fae66d
Austin Ginder <austinginder@gmail.com>
2023-02-28 14:46:00 -0500

    📦 NEW: Run class

---
+++ app/Run.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace CaptainCore;
+
+class Run {
+
+    protected $account_id = "";
+
+    public static function CLI( $command = "" ) {
+
+        if ( empty( $command ) ) {
+            return;
+        }
+        
+        // Disable https when debug enabled
+        if ( defined( 'CAPTAINCORE_DEBUG' ) ) {
+            add_filter( 'https_ssl_verify', '__return_false' );
+        }
+
+        $data = [ 
+            'timeout' => 45,
+            'headers' => [
+                'Content-Type' => 'application/json; charset=utf-8', 
+                'token'        => CAPTAINCORE_CLI_TOKEN 
+            ],
+            'body'        => json_encode( [ "command" => $command ]),
+            'method'      => 'POST',
+            'data_format' => 'body'
+        ];
+
+        // Add command to dispatch server
+        $response = wp_remote_post( CAPTAINCORE_CLI_ADDRESS . "/run", $data );
+        if ( is_wp_error( $response ) ) {
+            $error_message = $response->get_error_message();
+            return [];
+        }
+
+        return $response["body"];
+    }
+
+}
