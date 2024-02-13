8ea0984aadcdf4ee9ab13f4b975d85361f93b04c
Austin Ginder <austinginder@gmail.com>
2024-02-13 18:41:18 -0500

    👌 IMPROVE: Remove Mailgun package

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -7,7 +7,4 @@ $baseDir = dirname($vendorDir);
 
 return array(
     '6e3fae29631ef280660b3cdad06f25a8' => $vendorDir . '/symfony/deprecation-contracts/function.php',
-    '9c67151ae59aff4788964ce8eb2a0f43' => $vendorDir . '/clue/stream-filter/src/functions_include.php',
-    '8cff32064859f4559445b89279f3199c' => $vendorDir . '/php-http/message/src/filters.php',
-    'a4a119a56e50fbb293281d9a48007e0e' => $vendorDir . '/symfony/polyfill-php80/bootstrap.php',
 );
