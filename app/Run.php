2ecc6fef1958e087005f1bbba66b6c59cf74c07d
Austin Ginder <austinginder@gmail.com>
2024-04-26 10:27:22 -0400

    👌 IMPROVE: Site sync

--- app/Run.php
+++ app/Run.php
@@ -38,4 +38,39 @@ class Run {
         return $response["body"];
     }
 
+    public static function task( $command = "" ) {
+        
+        // Disable https when debug enabled
+        if ( defined( 'CAPTAINCORE_DEBUG' ) ) {
+            add_filter( 'https_ssl_verify', '__return_false' );
+        }
+    
+        $data = [
+            'timeout' => 45,
+            'headers' => [
+                'Content-Type' => 'application/json; charset=utf-8', 
+                'token'        => CAPTAINCORE_CLI_TOKEN 
+            ],
+            'body'        => json_encode( [ "command" => $command ]), 
+            'method'      => 'POST', 
+            'data_format' => 'body'
+        ];
+    
+        // Add command to dispatch server
+        $response = wp_remote_post( CAPTAINCORE_CLI_ADDRESS . "/tasks", $data );
+        if ( is_wp_error( $response ) ) {
+            $error_message = $response->get_error_message();
+            return "Something went wrong: $error_message";
+        }
+
+        $response = json_decode( $response["body"] );
+        
+        // Response with task id
+        if ( $response && $response->token ) { 
+            return $response->token; 
+        }
+    
+        return $response["body"];
+    }
+
 }
