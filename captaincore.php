ff8d3347befa99aa3b1c194e898dc41a71b9717e
Austin Ginder <austinginder@gmail.com>
2025-12-29 10:52:27 -0500

    📦 NEW: Password reset

--- captaincore.php
+++ captaincore.php
@@ -3922,69 +3922,17 @@ function captaincore_login_func( WP_REST_Request $request ) {
 			return;
 		}
 
-		$user_login = $user_data->user_login;
-		$user_email = $user_data->user_email;
-	
-		// Redefining user_login ensures we return the right case in the email.
-		$key        = get_password_reset_key( $user_data );
-	
+		// Generate the key
+		$key = get_password_reset_key( $user_data );
+
 		if ( is_wp_error( $key ) ) {
 			return $key;
 		}
-	
-		if ( is_multisite() ) {
-			$site_name = get_network()->site_name;
-		} else {
-			/*
-			 * The blogname option is escaped with esc_html on the way into the database
-			 * in sanitize_option we want to reverse this for the plain text arena of emails.
-			 */
-			$site_name = wp_specialchars_decode( get_option( 'blogname' ), ENT_QUOTES );
-		}
-	
-		$message = __( 'Someone has requested a password reset for the following account:' ) . "\r\n\r\n";
-		/* translators: %s: site name */
-		$message .= sprintf( __( 'Site Name: %s' ), $site_name ) . "\r\n\r\n";
-		/* translators: %s: user login */
-		$message .= sprintf( __( 'Username: %s' ), $user_login ) . "\r\n\r\n";
-		$message .= __( 'If this was a mistake, just ignore this email and nothing will happen.' ) . "\r\n\r\n";
-		$message .= __( 'To reset your password, visit the following address:' ) . "\r\n\r\n";
-		$message .= '<' . network_site_url( "wp-login.php?action=rp&key=$key&login=" . rawurlencode( $user_login ), 'login' ) . ">\r\n";
-	
-		/* translators: Password reset notification email subject. %s: Site title */
-		$title = sprintf( __( '[%s] Password Reset' ), $site_name );
-	
-		/**
-		 * Filters the subject of the password reset email.
-		 *
-		 * @since 2.8.0
-		 * @since 4.4.0 Added the `$user_login` and `$user_data` parameters.
-		 *
-		 * @param string  $title      Default email title.
-		 * @param string  $user_login The username for the user.
-		 * @param WP_User $user_data  WP_User object.
-		 */
-		$title = apply_filters( 'retrieve_password_title', $title, $user_login, $user_data );
-	
-		/**
-		 * Filters the message body of the password reset mail.
-		 *
-		 * If the filtered message is empty, the password reset email will not be sent.
-		 *
-		 * @since 2.8.0
-		 * @since 4.1.0 Added `$user_login` and `$user_data` parameters.
-		 *
-		 * @param string  $message    Default mail message.
-		 * @param string  $key        The activation key.
-		 * @param string  $user_login The username for the user.
-		 * @param WP_User $user_data  WP_User object.
-		 */
-		$message = apply_filters( 'retrieve_password_message', $message, $key, $user_login, $user_data );
-	
-		if ( $message && ! wp_mail( $user_email, wp_specialchars_decode( $title ), $message ) ) {
-			wp_die( __( 'The email could not be sent.' ) . "<br />\n" . __( 'Possible reason: your host may have disabled the mail() function.' ) );
-		}
 
+		// Use the Custom CaptainCore Mailer
+		CaptainCore\Mailer::send_password_reset( $user_data, $key );
+
+		return true;
 	}
 
 	if ( $post->command == "signIn" ) {
