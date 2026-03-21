cc42c2ac59d10f4432c151c901cf6635caf3d5cc
Austin Ginder <austinginder@gmail.com>
2026-03-21 13:42:58 -0400

    👌 IMPROVE: Domain provider sync

--- app/Providers/Hoverdotcom.php
+++ app/Providers/Hoverdotcom.php
@@ -16,4 +16,51 @@ class Hoverdotcom {
         }
     }
 
+    public static function login() {
+        $data = [
+            'timeout' => 45,
+            'headers' => [
+                'Content-Type' => 'application/json; charset=utf-8'
+            ],
+            'body'        => json_encode( [
+                "username" => self::credentials("username"),
+                'password' => self::credentials("password")
+            ] ),
+            'method'      => 'POST',
+            'data_format' => 'body'
+        ];
+
+        $response    = wp_remote_post( "https://www.hover.com/api/login", $data );
+        $cookie_data = json_encode( $response["cookies"] );
+        set_transient( 'captaincore_hovercom_auth', $cookie_data, HOUR_IN_SECONDS * 48 );
+    }
+
+    public static function fetch_domains() {
+        if ( empty( get_transient( 'captaincore_hovercom_auth' ) ) ) {
+            self::login();
+        }
+        $auth = get_transient( 'captaincore_hovercom_auth' );
+        $args = [
+            'timeout' => 45,
+            'headers' => [
+                'Cookie' => 'hoverauth=' . $auth
+            ]
+        ];
+
+        $response = wp_remote_get( "https://www.hover.com/api/control_panel/domains", $args );
+        if ( is_wp_error( $response ) ) {
+            return [];
+        }
+
+        $results = [];
+        $domains = json_decode( $response['body'] )->domains ?? [];
+        foreach ( $domains as $domain ) {
+            $results[] = (object) [
+                "name"   => $domain->name,
+                "status" => $domain->status,
+            ];
+        }
+        return $results;
+    }
+
 }
--- app/Providers/Spaceship.php
+++ app/Providers/Spaceship.php
@@ -16,4 +16,24 @@ class Spaceship {
         }
     }
 
+    public static function fetch_domains() {
+        $results = [];
+        $skip    = 0;
+        $take    = 100;
+        do {
+            $response = \CaptainCore\Remote\Spaceship::get( "domains", [ "take" => $take, "skip" => $skip ] );
+            if ( empty( $response->items ) ) {
+                break;
+            }
+            foreach ( $response->items as $item ) {
+                $results[] = (object) [
+                    "name"   => $item->name,
+                    "status" => $item->lifecycleStatus,
+                ];
+            }
+            $skip += $take;
+        } while ( count( $response->items ) == $take );
+        return $results;
+    }
+
 }
